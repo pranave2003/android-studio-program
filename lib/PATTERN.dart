a1b81675c1cb09044a5971f9ab1eec69c64de373
yadav-babu <139962239+yadav-babu@users.noreply.github.com>
2023-09-29 09:08:07 +0530

    2nd commit

---
+++ lib/PATTERN.dart
@@ -0,0 +1,13 @@
+import"dart:io";
+void main(){
+  String? n;
+  print("enter the row nymber");
+   n=stdin.readLineSync();
+  var b=int.parse(n!);
+  for(int i=0;i<b;i++) {
+    for (int j = 0; j<=i; j++) {
+      stdout.write("* ");
+    }
+    print("\n");
+  }
+}
